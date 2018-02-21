b0e53bf71323069ea9a9f764083f11b0d2b42208
pavelpopov <pavel.a.popov@onefactor.com>
2018-02-21 15:05:59 +0300

    crowler

--- crowler/src/main/java/com/nixira/crowler/App.java
+++ crowler/src/main/java/com/nixira/crowler/App.java
@@ -3,6 +3,9 @@ package com.nixira.crowler;
 import org.apache.commons.io.IOUtils;
 
 import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 /**
  * Created by pavel.a.popov on 12.02.18.
@@ -10,15 +13,66 @@ import java.net.URL;
 public class App {
     public static void main(String[] args) {
         try {
-            for (int i = 0; i < 10; i++) {
+            int errors = 0;
+            Set<String> inns = new HashSet<>();
+            while (inns.size() < 1000) {
+                String inn = getRandomString(10);
+                if (isValidINN(inn)) {
+                    inns.add(inn);
+                } else {
+                    errors++;
+                }
+            }
+            System.out.println(errors);
 
-                URL url = new URL("https://market.yandex.ru/product/1729208642");
+            for (String inn : inns) {
+
+                URL url = new URL(String.format("https://sbis.ru/contragents/%s", inn));
 
                 String content = IOUtils.toString(url.openStream(), "UTF-8");
-                System.out.println(content);
+                if (!content.contains("class=\"cCard__MainReq-Name\">Нет данных<")) {
+                    System.out.println(content);
+                }
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+
+    private static final Pattern innPatter = Pattern.compile("\\d{10}|\\d{12}");
+
+    public static boolean isValidINN(String inn) {
+        inn = inn.trim();
+        if (!innPatter.matcher(inn).matches()) {
+            return false;
+        }
+        int length = inn.length();
+        if (length == 12) {
+            return INNStep(inn, 2, 1) && INNStep(inn, 1, 0);
+        } else {
+            return INNStep(inn, 1, 2);
+        }
+    }
+
+    private static final int[] checkArr = new int[]{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8};
+
+    private static boolean INNStep(String inn, int offset, int arrOffset) {
+        int sum = 0;
+        int length = inn.length();
+        for (int i = 0; i < length - offset; i++) {
+            sum += (inn.charAt(i) - '0') * checkArr[i + arrOffset];
+        }
+        return (sum % 11) % 10 == inn.charAt(length - offset) - '0';
+    }
+
+    private static char[] chars = "0123456789".toCharArray();
+
+    private static String getRandomString(int len) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            sb.append(chars[(int) (Math.random() * (chars.length - 1))]);
+        }
+        return sb.toString();
+    }
+
 }
